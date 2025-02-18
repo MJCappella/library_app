98b314acb749080d6059996fc46171971deb9f37
MJCappella <mjaymickey001@gmail.com>
2025-02-18 09:23:56 +0300

    implemented logic for the lending model

--- app/models/lending.rb
+++ app/models/lending.rb
@@ -1,3 +1,17 @@
 class Lending < ApplicationRecord
   belongs_to :book
+
+  validates :borrower_name, presence: true
+  validates :borrowed_at, presence: true
+  validate :returned_at_after_borrowed_at
+
+  private
+
+  def returned_at_after_borrowed_at
+    return if returned_at.blank? || borrowed_at.blank?
+
+    if returned_at < borrowed_at
+      errors.add(:returned_at, "must be after borrowed_at")
+    end
+  end
 end
