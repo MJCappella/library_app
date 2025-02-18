7d1263c632a9e32f7070e2dc75fa8765c36f3551
MJCappella <mjaymickey001@gmail.com>
2025-02-18 09:54:12 +0300

    wrote model and controller tests for books

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,10 @@
+# controller test to get book indices
+
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get books_url
+    assert_response :success
+  end
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,10 @@
+# Should not save book without a title
+
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save book without title" do
+    book = Book.new(author: "Author", published_year: 2020)
+    assert_not book.save, "Saved the book without a title"
+  end
 end
